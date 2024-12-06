c32f2a4cbd40cd3d8e57fcb259ba09bcdac93898
lei.huang <lei.huang@gainstrong.cn>
2024-12-06 18:02:52 +0800

    ota_no_factory:添加简单的项目

---
+++ esp32/ota_no_factory/http_request/main/http_request_example_main.c
@@ -0,0 +1,41 @@
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+#include "sdkconfig.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_ota_ops.h"
+#include "esp_http_client.h"
+#include "esp_https_ota.h"
+#include "protocol_examples_common.h"
+#include "string.h"
+
+
+static const char *TAG = "simple_ota_example";
+
+
+
+
+void app_main(void)
+{
+
+
+
+}
